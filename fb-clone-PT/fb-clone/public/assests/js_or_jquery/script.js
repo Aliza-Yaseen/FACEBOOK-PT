184fbe1550c02a72789c75c051be9726ae3966e1
I`m  a passionate individual with a determined vision to become a top-tier web developer. <160003469+Aliza-Yaseen@users.noreply.github.com>
2024-08-04 19:27:30 +0500

    Add files via upload

---
+++ fb-clone-PT/fb-clone/public/assests/js_or_jquery/script.js
@@ -0,0 +1,13 @@
+let cardWrap = document.getElementById('cardWrap');
+
+function openCard(){
+cardWrap.classList.toggle('open-menu');
+}
+// document.addEventListener('DOMContentLoaded', function() {
+//     const menuButton = document.querySelector('.menu-button');
+//     const cardWrap = document.getElementById('cardWrap');
+
+//     menuButton.addEventListener('click', function() {
+//         openCard.classList.toggle('open-menu');
+//     });
+// });
